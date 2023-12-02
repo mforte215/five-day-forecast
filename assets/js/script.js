4f7411ddaafa46fadc130f7a292730f8475c8dae
mforte215 <mark.fortee@gmail.com>
2023-12-02 07:02:43 -0500

    updated current weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,46 @@
-const PRESELECTED_CITIES = ["Philadelphia", "Chicago", "Washington DC", "Los Angeles", "New York", "San Francisco", "Seattle", "Miami", "Dallas", "Houston", "Phoenix"];
+const PRESELECTED_CITIES = ["Philadelphia", "Chicago", "Washington DC", "Los Angeles", "New York", "San Francisco", "Boston", "Miami"];
 
-
-
-const findLatAndLonByName = async (cityName) => {
+const searchForm = document.querySelector('.search-form');
+const mainDisplay = document.getElementById("main-display");
+const onLoadCityData = async (cityName) => {
+    mainDisplay.style.display = "none";
     let newCorridnates = null;
-
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + API_KEY.id, {
+    let foundCityWeatherData = {
+        mainCurrentWeather: '',
+        mainCurrentTemp: '',
+        mainCurrentMinTemp: '',
+        mainCurrentMaxTemp: '',
+        mainCurrentWind: '',
+        mainCurrentHumidity: '',
+        fiveDayForecast: [{
+            mainWeather: '',
+            minTemp: '',
+            maxTemp: '',
+        },
+        {
+            mainWeather: '',
+            minTemp: '',
+            maxTemp: '',
+        },
+        {
+            mainWeather: '',
+            minTemp: '',
+            maxTemp: '',
+        },
+        {
+            mainWeather: '',
+            minTemp: '',
+            maxTemp: '',
+        },
+        {
+            mainWeather: '',
+            minTemp: '',
+            maxTemp: '',
+        }
+        ]
+    };
+    //get the corrdinates
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + API_KEY.id, {
         'method': 'GET'
     }).then(response => response.json())
         .then(response => {
@@ -17,21 +52,47 @@ const findLatAndLonByName = async (cityName) => {
             }
             console.log("LOGGING IN FIND BY NAME");
             console.log(newCorridnates);
+            //get the current weather
             fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
                 .then(response => {
                     console.log("Logging WEATHER FOR " + cityName);
-                    console.log(response.main);
+
+                    //get main current weather
+                    let mainCurrentWeather = response.weather[0].main;
+                    foundCityWeatherData.mainCurrentTemp = response.main.temp
+                    foundCityWeatherData.mainCurrentMinTemp = response.main.temp_min;
+                    foundCityWeatherData.mainCurrentMaxTemp = response.main.temp_max;
+                    foundCityWeatherData.mainCurrentWind = response.wind.speed;
+                    foundCityWeatherData.mainCurrentHumidity = response.main.humidity;
+
+                    document.querySelector('#current-temp-lbl').textContent = "Temp: " + foundCityWeatherData.mainCurrentTemp + "\u00B0F"
+                    document.querySelector('#current-wind-lbl').textContent = "Wind: " + foundCityWeatherData.mainCurrentWind + "MPH"
+                    document.querySelector('#current-humidity-lbl').textContent = "Humidity: " + foundCityWeatherData.mainCurrentHumidity + "%";
+
+                    console.log(response);
+                    document.querySelector(".main-current-weather-label").textContent = mainCurrentWeather;
+                    //get the five day forecast
+                    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
+                        .then(response => {
+                            console.log("Logging 5 day forecast for " + cityName);
+                            console.log(response);
+
+
+                            //set the found weather day and city name into the page.
+                            let cityNameHeader = document.querySelector('.current-weather-city-header');
+                            cityNameHeader.textContent = cityName;
+
+
+                            mainDisplay.style.display = "unset";
+
+                        })
+
+
                 })
 
 
         })
 
-}
-
-const onLoadCityData = async (cityName) => {
-    let corrdinates = await findLatAndLonByName(cityName);
-    console.log("LOGGING LOCATION IN ON LOAD");
-    console.log(corrdinates);
 }
 
 const setInitialData = () => {
@@ -40,7 +101,6 @@ const setInitialData = () => {
 
 const createPresetButtons = () => {
     //find the correct place to put them
-    const searchForm = document.querySelector('.search-form');
     let newContainer = document.createElement("ul");
     newContainer.id = "preset-container"
     newContainer.classList.add("preset-container");
@@ -75,4 +135,11 @@ setInitialData();
 
 
 
-
+searchForm.addEventListener("submit", (event) => {
+    //get input value 
+    event.preventDefault();
+    let citySearch = document.getElementById('city-name');
+    console.log("Logging got input");
+    console.log(citySearch.value);
+    onLoadCityData(citySearch.value);
+});
