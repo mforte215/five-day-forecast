77781cbc7b785a0af4aca5a2df83984e4d41cd11
mforte215 <mark.fortee@gmail.com>
2023-12-03 13:11:34 -0500

    five day forecast data update. icons complete. css recoloring

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,13 +56,18 @@ const onLoadCityData = async (cityName) => {
                 lat: foundLat,
                 lon: foundLon,
             }
-            console.log("LOGGING IN FIND BY NAME");
-            console.log(response[0]);
+
             let cityStateName = cityName + ", " + response[0].state;
+            //set the date
+            let weatherDateHeader = document.querySelector(".current-weather-date-header");
+            let today = dayjs();
+            let dateString = today.format('MM.DD.YYYY');
+            let dayString = today.format('dddd');
+            weatherDateHeader.textContent = "Current Weather For " + dayString + ", " + dateString;
+
             //get the current weather
             fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
                 .then(response => {
-                    console.log("Logging WEATHER FOR " + cityName);
 
                     //get main current weather
                     let mainCurrentWeather = response.weather[0].main;
@@ -72,7 +77,10 @@ const onLoadCityData = async (cityName) => {
                     foundCityWeatherData.mainCurrentWind = response.wind.speed;
                     foundCityWeatherData.mainCurrentHumidity = response.main.humidity;
                     foundCityWeatherData.mainCurrentFeelsLike = response.main.feels_like;
-
+                    console.log(response.weather[0]);
+                    let mainIconUrl = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+                    let mainIconImage = document.querySelector('#main-icon-image');
+                    mainIconImage.src = mainIconUrl;
                     document.querySelector('#current-temp-lbl').textContent = "Temp: " + foundCityWeatherData.mainCurrentTemp + "\u00B0F"
 
                     document.querySelector('#current-wind-lbl').textContent = "Wind: " + foundCityWeatherData.mainCurrentWind + "MPH"
@@ -85,16 +93,57 @@ const onLoadCityData = async (cityName) => {
 
                     document.querySelector('#current-feelslike-lbl').textContent = "Feels Like: " + foundCityWeatherData.mainCurrentFeelsLike + "\u00B0F";
 
-                    console.log(response);
                     document.querySelector(".main-current-weather-label").textContent = mainCurrentWeather;
                     //get the five day forecast
                     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
                         .then(response => {
-                            console.log("Logging 5 day forecast for " + cityName);
-                            console.log(response);
                             //set the 5 day forecast data
                             //Date/Time Index Values
+                            let foundDays = 0;
                             let forecastData = response.list;
+                            for (let j = 0; j < forecastData.length; j++) {
+
+                                //parse out values needed
+                                let dayWeatherObj = forecastData[j];
+                                let timeWeatherString = dayWeatherObj.dt_txt;
+                                let parsedtimeWeatherStringArray = timeWeatherString.split(" ");
+                                let forecastDate = parsedtimeWeatherStringArray[0];
+                                let forecastTime = parsedtimeWeatherStringArray[1];
+
+                                //set the date in the correct forecast tile and check the time of day to ensure noon forecast
+                                if (forecastTime === '15:00:00') {
+                                    //this is the correct forecast and needs to be captured into the tile
+                                    //set the current day first
+                                    let thisDay = dayjs(forecastDate);
+                                    //find correct label
+                                    console.log(dayWeatherObj);
+                                    let dayLabelString = ('#day-' + foundDays + '-forecast-date');
+                                    let dayLabel = document.querySelector('#day-' + foundDays + '-forecast-date');
+                                    dayLabel.innerText = thisDay.format('dddd');
+
+                                    let mainLbl = document.querySelector('#day-' + foundDays + '-forecast-main');
+                                    mainLbl.innerText = dayWeatherObj.weather[0].description;
+
+                                    let tempLbl = document.querySelector('#day-' + foundDays + '-forecast-temp');
+                                    tempLbl.innerText = Math.round(dayWeatherObj.main.temp) + "\u00B0F";
+
+                                    let windLbl = document.querySelector('#day-' + foundDays + '-forecast-wind');
+                                    windLbl.innerText = "Wind: " + dayWeatherObj.wind.speed + "MPH";
+                                    let humidLbl = document.querySelector('#day-' + foundDays + '-forecast-humidity');
+                                    humidLbl.innerText = "Humidity: " + dayWeatherObj.main.humidity + "%";
+
+                                    //set the icons
+                                    //create icon url
+                                    let iconUrl = "https://openweathermap.org/img/wn/" + dayWeatherObj.weather[0].icon + "@2x.png"
+                                    //change the src attr of image icon 
+                                    let weatherIcon = document.querySelector('#day-' + foundDays + '-forecast-image');
+                                    weatherIcon.src = iconUrl;
+                                    foundDays++;
+
+                                }
+
+
+                            }
 
                             //set the found weather day and city name into the page.
                             let cityNameHeader = document.querySelector('.current-weather-city-header');
@@ -157,7 +206,5 @@ searchForm.addEventListener("submit", (event) => {
     //get input value 
     event.preventDefault();
     let citySearch = document.getElementById('city-name');
-    console.log("Logging got input");
-    console.log(citySearch.value);
     onLoadCityData(citySearch.value);
 });
