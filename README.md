539e50f28f9889a69b5d1b49ecda79c6331f63a2
mforte215 <mark.fortee@gmail.com>
2023-12-05 09:58:05 -0500

    removal of outdated artifacts from repo. Added comments. Added favicon

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# five-day-forecast
+# five-day-forecast ⛈️
+
+A weather dashboard capable of showing you any city's weather in the world! The dashboard shows the current weather conditions given by city along with a five day forecast for the location. Included in the data is the min and max temps for the day. The humidity and wind speed. Along with the general overall conditions of the weather for the given time period.
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+The purpose of this site is to retrieve and display weather for any city in the world. It utilizes the [Open Weather Map API](https://openweathermap.org/api) which provides update-to-date information on the world's weather.
+
+Link to the website deployed on GitHub Pages: https://mforte215.github.io/five-day-forecast/
+
+Link to the code repository on GitHub: https://github.com/mforte215/five-day-forecast/
+
+A screenshot of the current site:
+![Screenshot of the current online site with the lay of the page displayed](./assets/images/weather_dashboard.png)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const PRESELECTED_CITIES = ["Philadelphia", "Chicago", "Washington DC", "Los Angeles", "New York", "San Francisco", "Boston", "Miami"];
-
+//Get static elements on the page
 const searchForm = document.querySelector('.search-form');
 const mainDisplay = document.getElementById("main-display");
 const splashDisplay = document.getElementById("splash-display");
 
+//Load Data when searching for city
 const onLoadCityData = async (cityName) => {
     splashDisplay.style.display = "block";
     document.querySelector(".splash-label").textContent = "LOADING...";
@@ -18,105 +18,86 @@ const onLoadCityData = async (cityName) => {
         mainCurrentFeelsLike: '',
         mainCurrentWind: '',
         mainCurrentHumidity: '',
-        fiveDayForecast: [{
-            mainWeather: '',
-            minTemp: '',
-            maxTemp: '',
-        },
-        {
-            mainWeather: '',
-            minTemp: '',
-            maxTemp: '',
-        },
-        {
-            mainWeather: '',
-            minTemp: '',
-            maxTemp: '',
-        },
-        {
-            mainWeather: '',
-            minTemp: '',
-            maxTemp: '',
-        },
-        {
-            mainWeather: '',
-            minTemp: '',
-            maxTemp: '',
-        }
-        ]
     };
-    //get the corrdinates
+    //get the corrdinates of a city by name. Limit 1.
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + API_KEY.id, {
         'method': 'GET'
     }).then(response => response.json())
         .then(response => {
-            let foundLat = response[0].lat;
-            let foundLon = response[0].lon;
-            newCorridnates = {
-                lat: foundLat,
-                lon: foundLon,
+            //endpoint returns empty array if no city was found
+            if (!response.length) {
+                document.querySelector(".splash-label").textContent = "Sorry, no results found. Please try again";
+
             }
+            else {
+                //found data for city successfully. Parse out relevant data and put it in the correct places
+                let foundLat = response[0].lat;
+                let foundLon = response[0].lon;
+                newCorridnates = {
+                    lat: foundLat,
+                    lon: foundLon,
+                }
+                let foundName = response[0].name;
+                let cityStateName = foundName + ", " + response[0].state;
+                //set the date
+                let weatherDateHeader = document.querySelector(".current-weather-date-header");
+                let today = dayjs();
+                let dateString = today.format('MM.DD.YYYY');
+                let dayString = today.format('dddd');
+                weatherDateHeader.textContent = "Current Weather For " + dayString + ", " + dateString;
 
-            let cityStateName = cityName + ", " + response[0].state;
-            //set the date
-            let weatherDateHeader = document.querySelector(".current-weather-date-header");
-            let today = dayjs();
-            let dateString = today.format('MM.DD.YYYY');
-            let dayString = today.format('dddd');
-            weatherDateHeader.textContent = "Current Weather For " + dayString + ", " + dateString;
+                //get the current weather
+                fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
+                    .then(response => {
+                        //found City, save it in localStorage;
 
-            //get the current weather
-            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
-                .then(response => {
 
-                    //get main current weather
-                    let mainCurrentWeather = response.weather[0].main;
-                    foundCityWeatherData.mainCurrentTemp = response.main.temp
-                    foundCityWeatherData.mainCurrentMinTemp = response.main.temp_min;
-                    foundCityWeatherData.mainCurrentMaxTemp = response.main.temp_max;
-                    foundCityWeatherData.mainCurrentWind = response.wind.speed;
-                    foundCityWeatherData.mainCurrentHumidity = response.main.humidity;
-                    foundCityWeatherData.mainCurrentFeelsLike = response.main.feels_like;
-                    console.log(response.weather[0]);
-                    let mainIconUrl = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
-                    let mainIconImage = document.querySelector('#main-icon-image');
-                    mainIconImage.src = mainIconUrl;
-                    document.querySelector('#current-temp-lbl').textContent = "Temp: " + foundCityWeatherData.mainCurrentTemp + "\u00B0F"
+                        //get main current weather
+                        let mainCurrentWeather = response.weather[0].main;
+                        foundCityWeatherData.mainCurrentTemp = response.main.temp
+                        foundCityWeatherData.mainCurrentMinTemp = response.main.temp_min;
+                        foundCityWeatherData.mainCurrentMaxTemp = response.main.temp_max;
+                        foundCityWeatherData.mainCurrentWind = response.wind.speed;
+                        foundCityWeatherData.mainCurrentHumidity = response.main.humidity;
+                        foundCityWeatherData.mainCurrentFeelsLike = response.main.feels_like;
+                        let mainIconUrl = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+                        let mainIconImage = document.querySelector('#main-icon-image');
+                        mainIconImage.src = mainIconUrl;
+                        document.querySelector('#current-temp-lbl').textContent = "Temp: " + foundCityWeatherData.mainCurrentTemp + "\u00B0F"
 
-                    document.querySelector('#current-wind-lbl').textContent = "Wind: " + foundCityWeatherData.mainCurrentWind + "MPH"
+                        document.querySelector('#current-wind-lbl').textContent = "Wind: " + foundCityWeatherData.mainCurrentWind + "MPH"
 
-                    document.querySelector('#current-humidity-lbl').textContent = "Humidity: " + foundCityWeatherData.mainCurrentHumidity + "%";
+                        document.querySelector('#current-humidity-lbl').textContent = "Humidity: " + foundCityWeatherData.mainCurrentHumidity + "%";
 
-                    document.querySelector('#current-minTemp-lbl').textContent = "Min Temp: " + foundCityWeatherData.mainCurrentMinTemp + "\u00B0F";
+                        document.querySelector('#current-minTemp-lbl').textContent = "Min Temp: " + foundCityWeatherData.mainCurrentMinTemp + "\u00B0F";
 
-                    document.querySelector('#current-maxTemp-lbl').textContent = "Min Temp: " + foundCityWeatherData.mainCurrentMaxTemp + "\u00B0F";
+                        document.querySelector('#current-maxTemp-lbl').textContent = "Max Temp: " + foundCityWeatherData.mainCurrentMaxTemp + "\u00B0F";
 
-                    document.querySelector('#current-feelslike-lbl').textContent = "Feels Like: " + foundCityWeatherData.mainCurrentFeelsLike + "\u00B0F";
+                        document.querySelector('#current-feelslike-lbl').textContent = "Feels Like: " + foundCityWeatherData.mainCurrentFeelsLike + "\u00B0F";
 
-                    document.querySelector(".main-current-weather-label").textContent = mainCurrentWeather;
-                    //get the five day forecast
-                    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
-                        .then(response => {
-                            //set the 5 day forecast data
-                            //Date/Time Index Values
-                            let foundDays = 0;
-                            let forecastData = response.list;
-                            for (let j = 0; j < forecastData.length; j++) {
+                        document.querySelector(".main-current-weather-label").textContent = mainCurrentWeather;
+                        //get the five day forecast
+                        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + newCorridnates.lat + '&lon=' + newCorridnates.lon + '&units=imperial&appid=' + API_KEY.id).then(response => response.json())
+                            .then(response => {
+                                //set the 5 day forecast data
+                                //Date/Time Index Values
+                                let foundDays = 0;
+                                let forecastData = response.list;
+                                for (let j = 7; j < forecastData.length; j += 8) {
 
-                                //parse out values needed
-                                let dayWeatherObj = forecastData[j];
-                                let timeWeatherString = dayWeatherObj.dt_txt;
-                                let parsedtimeWeatherStringArray = timeWeatherString.split(" ");
-                                let forecastDate = parsedtimeWeatherStringArray[0];
-                                let forecastTime = parsedtimeWeatherStringArray[1];
+                                    //parse out values needed
+                                    let dayWeatherObj = forecastData[j];
+                                    let timeWeatherString = dayWeatherObj.dt_txt;
+                                    let parsedtimeWeatherStringArray = timeWeatherString.split(" ");
+                                    let forecastDate = parsedtimeWeatherStringArray[0];
+                                    let forecastTime = parsedtimeWeatherStringArray[1];
 
-                                //set the date in the correct forecast tile and check the time of day to ensure noon forecast
-                                if (forecastTime === '15:00:00') {
+                                    //set the date in the correct forecast tile and check the time of day to ensure noon forecast
+
                                     //this is the correct forecast and needs to be captured into the tile
                                     //set the current day first
                                     let thisDay = dayjs(forecastDate);
                                     //find correct label
-                                    console.log(dayWeatherObj);
                                     let dayLabelString = ('#day-' + foundDays + '-forecast-date');
                                     let dayLabel = document.querySelector('#day-' + foundDays + '-forecast-date');
                                     dayLabel.innerText = thisDay.format('dddd');
@@ -143,30 +124,81 @@ const onLoadCityData = async (cityName) => {
                                 }
 
 
-                            }
 
-                            //set the found weather day and city name into the page.
-                            let cityNameHeader = document.querySelector('.current-weather-city-header');
-                            cityNameHeader.textContent = cityStateName;
+                                //set the found weather day and city name into the page.
+                                let cityNameHeader = document.querySelector('.current-weather-city-header');
+                                cityNameHeader.textContent = cityStateName;
+                                //add City Name to recently searched list
+                                //Need to check if it already exists 
 
-                            splashDisplay.style.display = "none";
-                            mainDisplay.style.display = "unset";
+                                let doesExist = false;
+                                for (let k = 0; k < localStorage.length; k++) {
+                                    let currentFoundCity = localStorage.getItem(k);
+                                    if (currentFoundCity == cityName) {
+                                        //City already exists in the list
+                                        doesExist = true;
+                                    }
+                                }
+                                // if city does not exist
+                                if (!doesExist) {
+                                    let newPresetItem = document.createElement("li");
+                                    newPresetItem.id = "preset-" + cityName;
+                                    newPresetItem.classList.add("preset-item");
+                                    newPresetItem.name = foundName;
+                                    newPresetItem.textContent = cityName;
+                                    newPresetItem.addEventListener("click", () => {
+                                        onLoadCityData(cityName);
+                                    })
+                                    //APPEND   
+                                    let preSearchedList = document.querySelector('#preset-container');
+                                    preSearchedList.append(newPresetItem);
+                                    localStorage.setItem(localStorage.length, cityName);
+                                }
 
-                        })
+                                splashDisplay.style.display = "none";
+                                mainDisplay.style.display = "unset";
+
+                            })
 
 
-                })
+                    })
 
-
+            }
         })
 
 }
 
+
+//Set the previously searched components from local storage
 const setInitialData = () => {
-    createPresetButtons();
+    //createPresetButtons();
+    // create the buttons from local storage and append
+    let preSearchedList = document.querySelector('#preset-container');
+
+    for (let i = 0; i < localStorage.length; i++) {
+
+        let foundCity = localStorage.getItem(i);
+        // create li 
+        let newPresetItem = document.createElement("li");
+        newPresetItem.id = "preset-" + foundCity;
+        newPresetItem.classList.add("preset-item");
+        newPresetItem.name = foundCity;
+        newPresetItem.textContent = foundCity;
+        newPresetItem.addEventListener("click", () => {
+            onLoadCityData(foundCity);
+        })
+
+        //append to list
+        preSearchedList.append(newPresetItem);
+    }
+
 }
 
-const createPresetButtons = () => {
+
+
+
+
+/* const createPresetButtons = () => {
     //find the correct place to put them
     let newContainer = document.createElement("ul");
     newContainer.id = "preset-container"
@@ -192,19 +224,19 @@ const createPresetButtons = () => {
 
     }
 
-
-
-
-
-}
+} */
 
 setInitialData();
 
 
-
+//Add event listener to search form for the load city data by name function 
 searchForm.addEventListener("submit", (event) => {
     //get input value 
     event.preventDefault();
     let citySearch = document.getElementById('city-name');
-    onLoadCityData(citySearch.value);
+    if (citySearch.value !== '') {
+        onLoadCityData(citySearch.value);
+        citySearch.value = '';
+    }
+
 });
